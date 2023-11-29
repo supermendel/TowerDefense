e5afbbb9dc593b408c10ae7ceb71b9a36b1f3220
supermendel <mendelgabi91@gmail.com>
2023-11-29 20:48:56 +0200

    Update TowerPlacement.cs

--- Assets/Scripts/Game/Buildings/Towers/TowerPlacement.cs
+++ Assets/Scripts/Game/Buildings/Towers/TowerPlacement.cs
@@ -119,7 +119,6 @@ public class TowerPlacement : MonoBehaviour
 			Destroy(towerPreviewInstance);
 			towerPreviewInstance = null;
             objectRenderer = null;
-			originalColor = null;
 
         }
 
